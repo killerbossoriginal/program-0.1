86d7052d3f98d195f8a8999872effa402caf8ade
killerbossoriginal <73131550+killerbossoriginal@users.noreply.github.com>
2021-03-07 19:42:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+function readLine() {
 var nome = imput("inserisci nome")
 var cognome = imput("inserisci cognome")
 var anno = imput("inserisci l'anno di nascita")
 var eta = 2020 - anno
 console.log("ti chiami " + nome + cognome)
 console.log("hai " + eta)
+}
